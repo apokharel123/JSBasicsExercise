bb68d3eae028d0335934a1652dba98e81fce212d
apokharel123 <Student@Bluegrasss-MacBook-Pro-2.local>
2022-02-28 21:46:24 -0500

    JavaScript Basics Exercise

---
+++ script.js
@@ -0,0 +1,76 @@
+//Homework
+// 1
+console.log(5);
+
+// 2
+
+/* Fiver main primitives are 
+1. Number, 
+2.String
+3.Boolean
+4.Undefined
+5.Null */
+
+//3
+/* Six operative sumbols are as follows:
+1. + (addition)
+2. - (subatraction)
+3. * (multiplication)
+4. / (divison)
+5. ** (Exponent)
+6. % (Modulus)
+# All of these follow PEMDAS rule */
+ 
+//4
+console.log(4%3);
+
+//5
+console.log('I\M MARY POPPINS,Y\'ALL!');
+console.log("I'M MARY POPPINS, Y'ALL!");
+
+//6
+console.log("HAWK"+ "EYE");
+
+//7
+console.log("AYUSHA POKHAREL".length);
+
+//8
+console.log("Avengers"[2]);
+console.log("Avengers"[5]);
+
+//Bonus
+// 1.
+console.log("\\");
+
+//2
+console.log((5**4)-9);
+
+//3
+console.log(("I".length)%1);
+console.log(("AM".length)%1);
+console.log(("Iron".length)%4);
+console.log(("Man".length)%3);
+console.log("I".length+"AM".length %"IRON".length-"MAN".length);
+
+//4
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("OKOYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+
+//5
+let m=25;
+let c=4;
+let u= m+c;
+console.log(u);
+
+
+
+
+
+
+
